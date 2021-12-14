e0e8fdf18b4c61e2df3cb8142eb1e116ef23e9a6
Mike Fix <mfix@stripe.com>
2021-12-14 09:21:51 -0800

    support next links OOTB

--- with-markdoc/runtime.js
+++ with-markdoc/runtime.js
@@ -1,6 +1,55 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const defaultTags = {
+  link: {
+    tag: 'Link',
+    description: 'Displays a Next.js link',
+    attributes: {
+      href: {
+        description: 'The path or URL to navigate to.',
+        type: String,
+        errorLevel: 'critical',
+        required: true,
+      },
+      as: {
+        description:
+          'Optional decorator for the path that will be shown in the browser URL bar.',
+        type: String,
+      },
+      passHref: {
+        description: 'Forces Link to send the href property to its child.',
+        type: Boolean,
+        default: false,
+      },
+      prefetch: {
+        description: 'Prefetch the page in the background.',
+        type: Boolean,
+        default: true,
+      },
+      replace: {
+        description:
+          'Replace the current history state instead of adding a new url into the stack.',
+        type: Boolean,
+        default: false,
+      },
+      scroll: {
+        description: 'Scroll to the top of the page after a navigation.',
+        type: Boolean,
+        default: true,
+      },
+      shallow: {
+        description:
+          'Update the path of the current page without rerunning getStaticProps, getServerSideProps or getInitialProps.',
+        type: Boolean,
+        default: true,
+      },
+      locale: {
+        description: 'The active locale is automatically prepended.',
+        type: Boolean,
+      },
+    },
+  },
   image: {
     tag: 'Image',
     description: 'Displays an image',
@@ -73,6 +122,7 @@ const defaultTags = {
 
 const defaultComponents = {
   Image,
+  Link,
 };
 
 export function transformSchema(schema) {
