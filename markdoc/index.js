766e931b6a8be786149c29237d1d10eacfd789cd
Mike Fix <mfix@stripe.com>
2022-02-03 14:53:41 -0800

    remove markdoc/index.js

--- markdoc/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Button.markdoc.js';
-export * from './Callout.markdoc.js';
-export * from './Code.markdoc.js';
-export * from './Icon.markdoc.js';
-export * from './Next.markdoc.js';
