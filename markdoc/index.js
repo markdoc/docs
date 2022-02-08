7bfee671ec4d382a13cd8cbecb7db6f61e949b3b
Mike Fix <mfix@stripe.com>
2022-02-08 13:16:37 -0800

    Revert "remove markdoc/index.js"
    This reverts commit 766e931b6a8be786149c29237d1d10eacfd789cd.

---
+++ markdoc/index.js
@@ -0,0 +1,6 @@
+export * from './Button.markdoc.js';
+export * from './Callout.markdoc.js';
+export * from './Code.markdoc.js';
+export * from './Heading.markdoc.js';
+export * from './Icon.markdoc.js';
+export * from './Next.markdoc.js';
