35ad2a2e5e2bd54d4cedc2ebbced0839f2056e16
Mike Fix <mfix@stripe.com>
2022-02-16 12:25:26 -0800

    position header anchors better

--- components/Heading.js
+++ components/Heading.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
 
-export function Heading({id = '', level = 1, children}) {
-  return React.createElement(`h${level}`, {id}, children);
+export function Heading({ id = '', level = 1, children }) {
+  return React.createElement(`h${level}`, { className: 'heading' }, [
+    React.createElement('div', { id, key: 'anchor', className: 'anchor' }),
+    children,
+  ]);
 }
 
 Heading.displayName = 'Heading';
