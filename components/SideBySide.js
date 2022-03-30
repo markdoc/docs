e615a9be1fb644012123da40dbba27252b176739
Mike Fix <mfix@stripe.com>
2022-03-30 14:40:11 -0700

    dark mode tweaks

--- components/SideBySide.js
+++ components/SideBySide.js
@@ -4,7 +4,7 @@ export function SideBySide({ children }) {
   const [first, ...rest] = React.Children.toArray(children);
   return (
     <div
-      className="flex row full-width side-by-side"
+      className="side-by-side flex row full-width"
       style={{
         padding: 0,
         marginTop: '1.5rem',
@@ -12,17 +12,16 @@ export function SideBySide({ children }) {
       }}
     >
       <div
-        className="flex column equal-width"
+        className="left flex column equal-width"
         style={{
           paddingTop: '0.75rem',
-          paddingRight: '3rem',
-          borderRight: '1px solid var(--side-by-side-border)'
+          paddingRight: '3rem'
         }}
       >
         {first}
       </div>
       <div
-        className="flex column equal-width"
+        className="right flex column equal-width"
         style={{ paddingTop: '0.75rem', paddingLeft: '3rem' }}
       >
         {rest}
