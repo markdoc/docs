f7ff0a3c56a581749b3aa80b4b57bbaeca45c2b9
Mike Fix <mfix@stripe.com>
2022-03-30 17:25:30 -0700

    adjust blockquote max-width

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,8 @@ title: Markdoc is a powerful, flexible Markdown-based authoring framework
 Markdoc is a powerful, flexible Markdown-based authoring framework.
 {% /typewriter %}
 
-> From personal blogs to massive documentation sites, Markdoc is a content authoring system that grows with you.
+> From personal blogs to massive documentation sites,  
+Markdoc is a content authoring system that grows with you.
 
 [Get started&nbsp;→](/docs/getting-started) {% .primary %}
 
