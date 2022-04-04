57beb6dc43ecd424675e30b643edf3e52dd95795
Mike Fix <mfix@stripe.com>
2022-04-04 13:10:08 -0700

    fancy heading and icon too

--- components/FancyHeading.js
+++ components/FancyHeading.js
@@ -2,51 +2,54 @@ import React from 'react';
 
 function Cover({ top, left, bottom, right }) {
   return (
-    <div
-      style={{
-        position: 'absolute',
-        width: 10,
-        height: 10,
-        background: 'var(--contrast-gray)',
-        top,
-        left,
-        bottom,
-        right
-      }}
-    />
+    <div>
+      <style jsx>
+        {`
+          div {
+            position: absolute;
+            width: 10px;
+            height: 10px;
+            background: var(--contrast-gray);
+            top: ${top}px;
+            left: ${left}px;
+            bottom: ${bottom}px;
+            right: ${right}px;
+          }
+        `}
+      </style>
+    </div>
   );
 }
 
 export function FancyHeading({ title, children }) {
   return (
-    <div
-      className="flex center-vertical"
-      style={{
-        position: 'relative',
-        padding: '3rem 4rem',
-        borderColor: 'var(--dark)',
-        borderWidth: '1px 1px 2px',
-        borderStyle: 'solid solid dashed'
-      }}
-    >
+    <div className="flex center-vertical">
       <Cover top={-5} left={-5} />
       <Cover bottom={-5} left={-5} />
       <Cover top={-5} right={-5} />
       <Cover bottom={-5} right={-5} />
-      <h2
-        style={{
-          fontFamily: 'var(--serif)',
-          fontSize: '43px',
-          fontWeight: 400,
-          lineHeight: '49px',
-          marginTop: 0,
-          paddingRight: '3rem',
-          whiteSpace: 'nowrap'
-        }}
-      >
-        {title}
-      </h2>
+      <h2>{title}</h2>
       {children}
+      <style jsx>
+        {`
+          .center-vertical {
+            position: relative;
+            padding: 3rem 4rem;
+            border-color: var(--dark);
+            border-width: 1px 1px 2px;
+            border-style: solid solid dashed;
+          }
+          h2 {
+            font-family: var(--serif);
+            font-size: 43px;
+            font-weight: 400;
+            line-height: 49px;
+            margin-top: 0;
+            padding-right: 3rem;
+            white-space: nowrap;
+          }
+        `}
+      </style>
     </div>
   );
 }
--- components/Icon.js
+++ components/Icon.js
@@ -16,22 +16,25 @@ const svgs = {
   ),
   'checkmark-circle': (
     <div className="relative">
-      <div
-        style={{
-          top: 2,
-          left: 2,
-          zIndex: 1,
-          position: 'absolute',
-          background: 'var(--black)',
-          borderRadius: '50%',
-          width: 12,
-          height: 12
-        }}
-      />
+      <div className="background" />
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <title>Checkmark Circle</title>
         <path d="M256 48C141.31 48 48 141.31 48 256s93.31 208 208 208 208-93.31 208-208S370.69 48 256 48zm108.25 138.29l-134.4 160a16 16 0 01-12 5.71h-.27a16 16 0 01-11.89-5.3l-57.6-64a16 16 0 1123.78-21.4l45.29 50.32 122.59-145.91a16 16 0 0124.5 20.58z" />
       </svg>
+      <style jsx>
+        {`
+          .background {
+            top: 2px;
+            left: 2px;
+            z-index: 1;
+            position: absolute;
+            background: var(--black);
+            border-radius: 50%;
+            width: 12px;
+            height: 12px;
+          }
+        `}
+      </style>
     </div>
   ),
   'copy-outline': (
@@ -75,22 +78,21 @@ const svgs = {
 
 export function Icon({ icon, color = 'inherit' }) {
   return (
-    <span
-      className="icon"
-      style={{
-        display: 'inline-block',
-        position: 'relative',
-        fontSize: 'inherit',
-        width: '1em',
-        height: '1em',
-        minWidth: '16px',
-        boxSizing: 'content-box',
-        color
-      }}
-    >
+    <span className="icon">
       {svgs[icon] || null}
       <style jsx>
         {`
+          .icon {
+            display: inline-block;
+            position: relative;
+            font-size: inherit;
+            width: 1em;
+            height: 1em;
+            min-width: 16px;
+            box-sizing: content-box;
+            color: ${color};
+          }
+
           .icon :global(svg) {
             z-index: 10; // HACK
             position: relative;
