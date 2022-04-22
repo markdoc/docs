0b1fbed02592f8573cfcbc0387e474eddb343826
Mike Fix <62121649+mfix-stripe@users.noreply.github.com>
2022-04-22 10:15:56 -0700

    update node example (#15)

--- examples/react-nodejs/content/example.md
+++
@@ -1,9 +0,0 @@
----
-route: '/examples'
----
-
-This is an example doc.
-
-```js
-const x = 'test';
-```
--- examples/react-nodejs/schema/heading.markdoc.js
+++ examples/react-nodejs/schema/heading.markdoc.js
@@ -1,4 +1,4 @@
-import { Heading } from '../../components/Heading';
+const { nodes } = require('@markdoc/markdoc');
 
 function getAnchor(children, attributes) {
   if (attributes.id && typeof attributes.id === 'string') {
@@ -12,21 +12,15 @@ function getAnchor(children, attributes) {
     .toLowerCase();
 }
 
-export default {
-  render: Heading,
-  children: ['inline'],
-  attributes: {
-    id: { type: String },
-    level: { type: Number, required: true, default: 1 },
-    className: { type: String }
-  },
+module.exports = {
+  ...nodes.heading,
   transform(node, config) {
     const attributes = node.renderAttributes(this.attributes);
     const children = node.renderChildren(config);
     const id = getAnchor(children, attributes);
 
     return {
-      name: this.render,
+      name: this.render(node),
       attributes: { ...attributes, id },
       children
     };
