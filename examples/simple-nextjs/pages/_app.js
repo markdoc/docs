c8dd6f64deded37c6818a5a3328e27fb627898ff
Charlie Gerard <charliegerard@stripe.com>
2022-04-22 11:55:46 -0700

    make it even smaller

--- examples/simple-nextjs/pages/_app.js
+++ examples/simple-nextjs/pages/_app.js
@@ -1,15 +1,5 @@
 import '../public/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <footer>
-        This is a boilerplate for Markdoc by{' '}
-        <a href="https://twitter.com/StripeDev">@StripeDev</a>
-      </footer>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
