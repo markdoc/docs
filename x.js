0e121b7e5ffb3f034e83aa9a64e41ec0aeebd14e
Mike Fix <mfix@stripe.com>
2022-04-27 16:20:59 -0700

    remove x.js

--- x.js
+++
@@ -1,7 +0,0 @@
-const Markdoc = require('@markdoc/markdoc');
-
-const source = '# Markdoc';
-const ast = Markdoc.parse(source);
-const content = Markdoc.transform(ast);
-
-console.log(Markdoc.renderers.html(content));
