72f965ef89a9c952ad5c13fa84f2a7cddb931c71
Mike Fix <mfix@stripe.com>
2022-04-29 14:34:32 -0700

    link to Headings

--- components/Heading.js
+++ components/Heading.js
@@ -1,19 +1,27 @@
 import * as React from 'react';
 
 export function Heading({ id = '', level = 1, children, className }) {
-  return React.createElement(
-    `h${level}`,
-    { className: ['heading', className].filter(Boolean).join(' ') },
-    [
-      React.createElement('div', {
-        id,
-        key: 'anchor',
-        style: {
-          position: 'absolute',
-          top: 'calc(-1 * (var(--nav-height) + 44px))'
-        }
-      }),
-      children
-    ]
+  const Component = `h${level}`;
+  return (
+    <a href={level === 1 ? '' : `#${id}`}>
+      <Component className={['heading', className].filter(Boolean).join(' ')}>
+        <div id={id} />
+        {children}
+      </Component>
+      <style jsx>
+        {`
+          a {
+            text-decoration: none;
+          }
+          a:hover {
+            opacity: 1;
+          }
+          div {
+            position: absolute;
+            top: calc(-1 * (var(--nav-height) + 44px));
+          }
+        `}
+      </style>
+    </a>
   );
 }
