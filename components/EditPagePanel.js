a8804003b405d3331c69a2fc680c6d24ec3a48fd
Mike Fix <mfix@stripe.com>
2022-05-06 11:20:39 -0700

    remove live-edit buttons on smaller screens

--- components/EditPagePanel.js
+++ components/EditPagePanel.js
@@ -110,6 +110,9 @@ export function EditPagePanel({ children }) {
             section {
               display: none;
             }
+            :global(.live-edit) {
+              display: none;
+            }
           }
         `}
       </style>
