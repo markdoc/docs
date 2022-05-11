0b00a3bcd1c4fe7aba85dcdbc1abbaa6195fa918
Mike Fix <mfix@stripe.com>
2022-05-11 07:02:10 -0700

    redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const withMarkdoc = require('@markdoc/next.js');
 module.exports = withMarkdoc()({
   reactStrictMode: true,
   pageExtensions: ['js', 'md', 'mdoc'],
+  redirects() {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/getting-started',
+        permanent: false
+      }
+    ];
+  },
   rewrites() {
     return [
       {
