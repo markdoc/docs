b1af65186c96be4673fa0583abac71eb62e4ac50
Mike Fix <62121649+mfix-stripe@users.noreply.github.com>
2022-05-18 09:08:10 -0700

    use i18n config instead of _document (#58)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withMarkdoc = require('@markdoc/next.js');
 module.exports = withMarkdoc()({
   reactStrictMode: true,
   pageExtensions: ['js', 'md', 'mdoc'],
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  },
   redirects() {
     return [
       {
