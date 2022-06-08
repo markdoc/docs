02c93873cbeb2b1fae748a374c593d6d7a51e1c7
Mike Fix <mfix@stripe.com>
2022-06-08 10:11:12 -0700

    hardcode index name

--- components/Shell/TopNav.js
+++ components/Shell/TopNav.js
@@ -7,8 +7,8 @@ function Search() {
   return (
     <DocSearch
       appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}
-      indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME}
       apiKey={process.env.NEXT_PUBLIC_ALGOLIA_API_KEY}
+      indexName="markdoc"
     />
   );
 }
